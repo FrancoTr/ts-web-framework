7ae20c8ca3ea10e541ea98780debb2b08cc76166
Franco Traverso <franco-t20@outlook.com>
2023-10-17 20:36:17 -0300

    Add user class setter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "nametest123", age: 77 });
+
+user.set({ name: "newnametest" });
 console.log(user.get("name"));
 console.log(user.get("age"));
