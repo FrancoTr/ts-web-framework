753eaf31f078d5bb3a7f21ed8a06ac0c249d9faa
Franco Traverso <franco-t20@outlook.com>
2023-11-01 23:41:37 -0300

    Add sync class

---
+++ src/models/Sync.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from "axios";
+import { UserProps } from "./User";
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+  fetch(): void {
+    axios.get(`${this.rootUrl}/${this.get("id")}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const id = this.get("id");
+    if (id) {
+      axios.put(`${this.rootUrl}/${id}`, this.data);
+    } else {
+      axios.post(this.rootUrl, this.data);
+    }
+  }
+}
