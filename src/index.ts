0a856dc6475907ff9afbd3dae606bcc822eff71d
Franco Traverso <franco-t20@outlook.com>
2023-11-13 22:25:01 -0300

    Test new user creation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, name: "newer name", age: 0 });
+const user = User.buildUser({ id: 5 });
 
 user.on("change", () => {
   console.log("new user: ", user);
 });
 
-user.save();
+user.fetch();
