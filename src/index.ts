17121876730d3fefec5331bb1e0ba7edbe9342f6
Franco Traverso <franco-t20@outlook.com>
2023-11-22 20:49:42 -0300

    Add user data into user form

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
+
+const user = User.buildUser({ name: "NAME1", age: 30 });
 
 const rootElement = document.getElementById("root") as HTMLElement;
-const userForm = new UserForm(rootElement);
+const userForm = new UserForm(rootElement, user);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: HTMLElement) {}
+  constructor(public parent: HTMLElement, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -14,6 +16,8 @@ export class UserForm {
     return `
         <div>
             <h1>User Form</h1>
+            <div>User name: ${this.model.get("name")}</div>
+            <div>User age: ${this.model.get("age")}</div>
             <input />
         </div>
         `;
