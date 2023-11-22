f744278210d1941e3269a67e23c22bb769e66529
Franco Traverso <franco-t20@outlook.com>
2023-11-21 22:47:05 -0300

    Bind event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,15 @@
 export class UserForm {
   constructor(public parent: HTMLElement) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick,
+    };
+  }
+  onButtonClick(): void {
+    console.log("onClick");
+  }
+
   template(): string {
     return `
         <div>
@@ -10,9 +19,23 @@ export class UserForm {
         `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
