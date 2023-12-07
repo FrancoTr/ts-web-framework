3f209ce086f3e2b35bc26ca47a97c7954e897f1c
Franco Traverso <franco-t20@outlook.com>
2023-12-06 21:25:31 -0300

    Add map regions method

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,10 +1,15 @@
 import { HasId, Model } from "../models/Model";
 export abstract class View<T extends Model<K>, K extends HasId> {
+  regions: { [key: string]: Element } = {};
   constructor(public parent: HTMLElement, public model: T) {
     this.bindModel();
   }
 
   abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {};
@@ -28,6 +33,19 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
   render(): void {
     this.parent.innerHTML = "";
 
@@ -35,6 +53,7 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
